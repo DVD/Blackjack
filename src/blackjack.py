ebcd27cdc951185db2c9d0697f42ffd4e141d25b
Deyan Doychev <deyandoichev@gmail.com>
2010-01-24 16:48:27 +0200

    Fixed a lot of bugs in the dealer and player classes. Added a HumanPlayer class that interacts with the UI. Added functionality to the dealer - now it also deals its own cards :).

--- src/blackjack.py
+++ src/blackjack.py
@@ -1,6 +1,7 @@
 import card_utils
 from blackjack_messages import *
 from pysage import Actor, ActorManager
+from simpleui import *
 
 class GameTable(object):
     '''Represents the Blackjack table with the dealer and the players and the entire game'''
@@ -17,11 +18,13 @@ class GameTable(object):
         self.dealer = Dealer()
         self.round_number = 0
         self.ranking = []
-        self.is_eliminated = [ True ] * 7
+        self.is_eliminated = [ False ] * 7
         # this initialization must change
-        self.players = [ Player(_, 100) for _ in range(2) ]
+        self.players = [ HumanPlayer(str(i), 100) for i in range(1) ]
+        self.ui = SimpleUI()
         # register players and dealer as actors
         am = ActorManager.get_singleton()
+        am.register_actor(self.ui, 'ui')
         for player in self.players:
             am.register_actor(player, player.player_id)
         am.register_actor(self.dealer, 'dealer')
@@ -36,7 +39,9 @@ class GameTable(object):
         players = [player for player_num, player in enumerate(self.players) if not self.is_eliminated[player_num]]
         self.dealer.deal_cards(players)
         for player in players:
-            dealer.deal_with_player(player)
+            self.dealer.deal_with_player(player)
+        self.dealer.play()
+        self.round_number+=1
 
     def __update_database(self):
         '''Update the database with current game ranking'''
@@ -52,7 +57,7 @@ class Dealer(Actor):
         Actor.__init__(self)
         self.deck = card_utils.BlackjackDeck()
         self.subscriptions = [ 'DecisionResponse' ]
-        self.cards = card_utils.BlackjackHand()
+        self.hand = card_utils.BlackjackHand()
 
     def __ignore_handler(self):
         '''An empty handler used to skip some messages'''
@@ -60,31 +65,48 @@ class Dealer(Actor):
 
     def send_message(self, msg):
         msg.sender='dealer'
-        ActorManager.get_singleton().trigger(msg)
+        return ActorManager.get_singleton().trigger(msg)
 
     def ask_player_for_insurance(self, player):
         self.send_message(InsuranceOffer(player_id = player))
 
-    def deal_card(self, player_num, hand_num):
-
-        self.send_message(CardDeal(player_id = player_num, hand_number = hand_num, card=self.deck.pop(0)))
+    def deal_card(self, player_num, hand_num, card = None):
+        if not card:
+            card = self.deck.pop(0)
+        self.send_message(CardDeal(player_id = player_num, hand_number = hand_num, card = card))
 
     def deal_cards(self, players):
         for player in players:
-            self.deal_card(players[player].player_id, 0)
-        self.deal_card('dealer', 0)
+            self.deal_card(player.player_id, 0)
+        card = self.deck.pop(0)
+        self.hand.add_card(card)
+        self.deal_card('dealer', 0, card)
         for player in players:
-            self.deal_card(players[player].player_id, 0)
+            self.deal_card(player.player_id, 0)
 
     def deal_with_player(self, player):
+        self.cuurent_player = player.player_id
         for (hand_num, hand) in enumerate(player.hands):
-            self.send_message(PlayerHandTurn(player_id = player.player_id, hand_number = 0))
+            self.current_hand = hand_num
+            self.send_message(PlayerHandTurn(player_id = player.player_id, hand_number = hand_num))
             while (hand.status == 'not played'):
                 self.send_message(DecisionRequest(player_id = player.player_id, hand_number = hand_num))
 
     def handle_DecisionResponse(self, msg):
         if (msg.get_property('action') == 'hit'):
-            send_message(CardDeal(player_id = player, hand_number = msg.get_property('hand_number')))
+            self.send_message(
+                    CardDeal(player_id = self.cuurent_player, hand_number = self.current_hand, card = self.deck.deal()))
+
+    def play(self):
+        while len(self.hand.sum) > 0 and ( min(self.hand.sum) < 17 or ( min(self.hand.sum) == 17 and len(self.hand.sum) > 1) ):
+            card = self.deck.pop(0)
+            self.hand.add_card(card)
+            self.deal_card('dealer', 0, card)
+
+        if self.hand.is_blackjack():
+            self.send_message(BlackjackAnnouncement(hand_number = 0))
+        if self.hand.is_busted():
+            self.send_message(BustAnnouncement(hand_number = 0))
 
 
 class Player(Actor):
@@ -117,21 +139,47 @@ class Player(Actor):
         self.hands[hand_number].add_card(card)
         self.card_dealt_to_self(card, hand_number)
 
+        if self.hands[hand_number].is_blackjack():
+            self.send_message(BlackjackAnnouncement(hand_number = hand_number))
+        if self.hands[hand_number].status == 'bust':
+            self.send_message(BustAnnouncement(hand_number = hand_number))
+
     def handle_DecisionRequest(self, msg):
         action = self.decide()
         if action == 'stand':
             self.hands[msg.get_property('hand_number')].status = 'stand'
 
-        send_message(DecisionResponse(action = self.decide(), hand_number = msg.get_property('hand_number')))
+        self.send_message(DecisionResponse(action = self.decide(), hand_number = msg.get_property('hand_number')))
 
     def card_dealt_to_player(self, card):
         raise NotImplementedError()
 
-    def card_dealt_to_self(self, card):
+    def card_dealt_to_self(self, card, hand_number):
         raise NotImplementedError()
 
     def decide(self):
         raise NotImplementedError()
+
+
+class HumanPlayer(Player):
+    def __init__(self, player_id, money):
+        Player.__init__(self, player_id, money)
+        self.subscriptions.extend(['HumanDecisionResponse'])
+        self.action = 'hit'
+
+    def card_dealt_to_player(self, card):
+        pass
+
+    def card_dealt_to_self(self, card, hand_number):
+        pass
+
+    def decide(self):
+        self.send_message(HumanDecisionRequest(allowed_actions = [ 'hit', 'stand']))
+        return self.action
+
+    def handle_HumanDecisionResponse(self, msg):
+        if (msg.get_property('player_id') == self.player_id):
+            self.action = msg.get_property('action')
 
 
 if __name__ == '__main__':
